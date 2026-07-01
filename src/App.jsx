16926ad887549b6a9ddfca6a1b331a729798ec37
Adyasha <adyasha901@gmail.com>
2026-07-01 11:13:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,95 @@
+import Navbar from "./Layout/Navbar";
+import Footer from "./Layout/footer";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Menu from "./pages/Menu";
+import Pages from "./pages/Pages";
+import Contact from "./pages/Contact";
+import BookTable from "./Components/BookTable"
+import RegisterPage from "./Components/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import AdminPanel from "./pages/AdminPanel";
+import AdminProtectedRoute from "./pages/AdminProtectedRoute"
+import Dashboard from "./Components/Dashboard";
+import Food from "./Components/Food";
+import AddFoodPage from "./pages/AddFoodPage";
+import EditFoodPage from "./pages/EditFoodPage";
+import DetailsPage from "./pages/DetailsPage";
+import OrderPage from "./pages/OrderPage";
+import Booking from "./pages/Bookings";
+import Bookings from "./pages/Bookings";
+
+function Layout() {
+  const location = useLocation()
+
+  const isAdminRoute = location.pathname.startsWith("/admin");
+
+  return (
+    <>
+      {!isAdminRoute && <Navbar />}
+      <div style={{ marginTop: isAdminRoute ? "0px": "120px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/pages" element={<Pages />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/booktable" element={<BookTable />} />
+          <Route path="/get-details/:id" element={<DetailsPage />} />
+           <Route path="/order" element={<OrderPage />} />
+
+
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/admin" element={<AdminProtectedRoute />}>
+            <Route path="" element={<AdminPanel />}>
+              <Route
+                path="dashboard"
+                element={<Dashboard />}
+              />
+
+              <Route
+                path="food"
+                element={<Food />}
+              />
+
+               <Route
+                path="/admin/add-food"
+                element={<AddFoodPage />}
+              />
+
+              <Route
+                path="/admin/edit/:id"
+                element={<EditFoodPage/>}
+              />
+
+              <Route
+                path="/admin/booking"
+                element={<Bookings/>}
+              />
+            </Route>
+          
+          </Route>
+
+        </Routes>
+      </div>
+      {!isAdminRoute && <Footer />}
+    </>
+  )
+}
+
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
